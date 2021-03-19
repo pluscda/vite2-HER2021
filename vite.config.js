73def12f0f0933e4f2d808dc7e278d62970342b0
alex <pluscda@gmail.com>
2021-03-19 17:40:43 +0800

    g

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 const { resolve } = require('path');
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:"./",//打包路徑
   server: {
-    port: 8000, // 配置启用的端口号
+    port: 2000, // 配置启用的端口号
+    open: true,
+    proxy: {
+      // 選項寫法
+      '/api': 'http://123.56.85.24:5000'//代理網址
+    },
+    cors:true
   },
   resolve: {
     // 设置别名
     alias: {
+      '@': path.resolve(__dirname, 'src'),//設置別名
       '@/views': resolve(__dirname, 'src/views/'),
       '@/styles': resolve(__dirname, 'src/styles/'),
     },
