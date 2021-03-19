a818e11f6adef8987760a53e08fdcaffa60c3dfe
alex <pluscda@gmail.com>
2021-03-19 17:24:03 +0800

    g

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,14 @@
 <template>
   <button @click="inc">{{ count }}</button>
   <div>{{ sss }}</div>
+  <div>{{ computedMsg }}</div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 const count = ref(0);
 const inc = () => count.value++;
+const computedMsg = computed(() => count.value + "eee!!!");
 onMounted(() => {
   console.log("mounted in the composition api!");
   inc();
